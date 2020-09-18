9d9fe314334abf60bcb4dbc60b0ffd15272e3456
Kalpesh Tated <jainkalpesh123456@gmail.com>
2020-09-18 17:50:37 +0530

    'Created'

---
+++ public/precache-manifest.61a55f31632938315c84a2380ed01691.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faea5b959cdec22b3da49af3ba9bbfbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d75a2e34bb3003c8a71a",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "982097db4399771d0d07",
+    "url": "/static/js/2.78f92b60.chunk.js"
+  },
+  {
+    "revision": "1168987de8c022b65d0b713d94cc1242",
+    "url": "/static/js/2.78f92b60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75a2e34bb3003c8a71a",
+    "url": "/static/js/main.5024e84b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "d166dd2c61b99cc96fcf774e01ce1f53",
+    "url": "/static/media/logo.d166dd2c.png"
+  }
+]);
